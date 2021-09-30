54cfb8b1ed1da8234b7efd3c4de1e4ccb070fa2a
Lokesh-Kanna <86400424+Lokesh-Kanna@users.noreply.github.com>
2021-09-30 12:59:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+async function foo() {
+    let userInput = await document.getElementById('user-Input').value;
+    console.log(userInput);
+
+    let covidCountry = await fetch(`https://api.covid19api.com/dayone/country/${userInput}`);
+    let covidCountryData = await covidCountry.json();
+
+    for(let i = 0; i < covidCountryData.length; i++){
+        let covidConfirmed = covidCountryData[i].Confirmed;
+        let covidCountryCode = covidCountryData[i].CountryCode;
+        let covidDate = covidCountryData[i].Date;
+        let covidRecovered = covidCountryData[i].Recovered;
+        let covidDeath = covidCountryData[i].Deaths;
+        // console.log(`Country: ${userInput}
+        //              Country Code: ${covidCountryCode}
+        //              Date: ${covidDate}
+        //              Confirmed Cases: ${covidConfirmed}
+        //              Recovered Cases: ${covidRecovered}
+        //              Deaths: ${covidDeath}`);
+        let div = document.createElement('div');
+        div.innerHTML = `Country: ${userInput} <br>
+        Country Code: ${covidCountryCode} <br>
+        Date: ${covidDate} <br>
+        Confirmed Cases: ${covidConfirmed} <br>
+        Recovered Cases: ${covidRecovered} <br>
+        Deaths: ${covidDeath} <br> <hr>`
+        document.body.append(div);
+    }
+    
+}
